d42a1a993a020493fee84ccc7129b54184693947
AliPolat <alirizap.bayri@gmail.com>
2024-06-24 15:14:17 +0300

    disable github icon

--- main.py
+++ main.py
@@ -76,6 +76,19 @@ def match_template(image, template):
 
 # plt.show()
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 uploaded_file = st.sidebar.file_uploader(
     "Bir resim dosyası seçin", type=["png", "jpg", "jpeg"]
 )
